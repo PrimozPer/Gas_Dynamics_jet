a45dc3662b700b2c32ca69593bc2372651e3aaa7
PrimozPer <114294353+PrimozPer@users.noreply.github.com>
2025-10-01 22:23:24 +0200

    OK so we've reached a point where i dont know what im doing anymore

--- main.py
+++ main.py
@@ -86,6 +86,11 @@ def compute_mach_angle(nulist_fan, j, gamma, down):
     else:
         theta = mach_angle - philist_fan[j]
     return theta
+def get_entry(plot_list, fan, char):
+    for entry in plot_list:
+        if entry["fan"] == fan and entry["char"] == char:
+            return entry
+    raise KeyError(f"No entry found with fan={fan}, char={char}")
 
 def compute_fan_gamma_minus(theta, x_start, y_start, N_chars, philist_fan, nulist_fan, start_points, reflected,plot_list):
     if np.sin(theta) != 0:
@@ -117,17 +122,13 @@ def compute_fan_gamma_minus(theta, x_start, y_start, N_chars, philist_fan, nulis
         new_reflected.append(reflected[j])
     return new_reflected
 
-def compute_fan_gamma_plus(theta, x_start, y_start, N_chars, philist_fan, nulist_fan, start_points, reflected, shear_anchor,plot_list):
-    # -------- UPWARD case: intersect shear line --------
-    length = a * 5  # finite length
-    x_edge = shear_anchor[0] + length * np.cos(philist_fan[j])
-    y_edge = shear_anchor[1] + length * np.sin(philist_fan[j])
 
-    shear_entry = {
-        "theta": philist_fan[j],  # shear follows phi, not theta
-        "x0": shear_anchor[0], "y0": shear_anchor[1],
-        "x1": x_edge, "y1": y_edge,
-        "type": 2,
+def store_upwards_char_info(i,j,philist_fan,nulist_fan,plot_list):
+    entry = {
+        "theta": None,  # to be filled later
+        "x0": None, "y0": None,
+        "x1": None, "y1": None,
+        "type": 1,
         "fan": i,
         "char": j,
         "phi": philist_fan[j],
@@ -135,35 +136,106 @@ def compute_fan_gamma_plus(theta, x_start, y_start, N_chars, philist_fan, nulist
         "pair_key": j,
         "merged": False
     }
-    plot_list.append(shear_entry)
-    #get the flow angle for the current region in the fan
+    plot_list.append(entry)
+    return plot_list
 
-    phi_flow = philist_fan[j]
+def compute_reflection_from_shear_line(i,j,plot_list,shear_anchor):
+    #get first shear line from phi and last start point
+    #the shear anchor is the start of the last characteristic treated 
+    
+    #since the upwards fan is just computed and stored, we need to ste the index down by 1
+    i=i-1
+    # i is the index of the last upward fan
+    # i+1 is the index of the current downward fan
+    # i-1 is the previous downward fan
+    
+    if j==0: #special case for 1st char in fan
+        #get start of last char in previous fan
+        region = get_entry(plot_list, i-1, N_chars-1)
+        shear_anchor = (region['x0'], region['y0'])
+        
+        #get the flow angle for the current region in the fan
+        shear_line_angle = region['phi']
+        if debug:
+            print("shear angle for 1st char in fan:", shear_line_angle)
+    else:
+        #shear anchor is the end of the previous char in the current fan
+        shear_anchor = get_entry(plot_list, i, j-1)
+        shear_anchor = (shear_anchor['x1'], shear_anchor['y1'])
+        up_line = get_entry(plot_list, i, j)
+        #the shear is now the phi after the previous char (downwards) in the current fan
+        down_line= get_entry(plot_list, i+1, j-1)
+        shear_line_angle = 0.5*((up_line['nu'] + up_line['phi'])-(down_line['nu']+down_line['phi']))
+        if debug:
+            print("shear anchor for char ", j, " in fan:", shear_anchor)
+    #get the endpoint of the downward char in the previous fan
+    prev_char_start = get_entry(plot_list, i-1, j)
+    
+    x_start = prev_char_start['x1']
+    y_start = prev_char_start['y1'] #y should be 0
     
     
+    
+    #the nu of the upwards fan is required to compute the mach angle
+    #put in list to match the function input
+    nu = [get_entry(plot_list, i, j)['nu']]
+    theta = compute_mach_angle(nu, 0, gamma, down=False)  # angle of characteristic line
     if debug:
-        print("Fan number: ", i, "Char number: ", j, "Phi flow (deg): ", np.degrees(phi_flow), 'Theta: ', np.degrees(theta))
+        print("Fan number: ", i, "Char number: ", j, "Shear angle (deg): ", np.degrees(shear_line_angle), 'Theta: ', np.degrees(theta))
         print("Previous end points:", shear_anchor)
-    #make new line equation for shear line
-    A = np.array([[np.cos(theta), -np.cos(phi_flow)],
-                    [np.sin(theta), -np.sin(phi_flow)]])
-    #start the new shar equation at the ennd of the previous line
+    #solve for intersection of shear and characteristic line
+    A = np.array([[np.cos(theta), -np.cos(shear_line_angle)],
+                    [np.sin(theta), -np.sin(shear_line_angle)]])
+    #start the new shear equation at the end of the previous line
     b = np.array([shear_anchor[0]-x_start,shear_anchor[1]-y_start])
-
-
     try:
         t, s = np.linalg.solve(A, b)
         if t > 0 and s > 0:
             x_end = x_start + t * np.cos(theta)
             y_end = y_start + t * np.sin(theta)
-
+            #update the previously emptu upwards entry in plot list
+            entry = get_entry(plot_list, i, j)
+            entry["theta"] = theta
+            entry["x0"] = x_start
+            entry["y0"] = y_start
+            entry["x1"] = x_end
+            entry["y1"] = y_end
+            entry["type"] = 1
+            entry["fan"] = i
+            entry["char"] = j
+            entry["phi"] = entry["phi"] #dont change
+            entry["nu"] = entry["nu"] #dont change
+            entry["pair_key"] = j
+            entry["merged"] = False
+            plot_list.append(entry)
+        else:
+            if debug:
+                print("No valid intersection found for fan ", i, " char ", j)
+    except np.linalg.LinAlgError:
+        if debug:
+            print("No valid intersection found for fan ", i, " char ", j)
+    #now compute the downward char reflection
+    #shear anchor is at the end of the just computed upward char
+    #the new start point is the end of the previous upward char
+    x_start = x_end
+    y_start = y_end
+    theta = compute_mach_angle(nulist_fan, j, gamma, down=True)  # angle of characteristic line
+    if debug:
+        print("Fan number: ", i+1, "Char number: ", j, "Shear angle (deg): ", np.degrees(shear_line_angle), 'Theta: ', np.degrees(theta))
+        print("Previous end points:", shear_anchor)
+    #solve for intersection with y=0
+    if np.sin(theta) != 0:
+        t = y_start / np.sin(theta)
+        if t > 0:
+            x_end = x_start + t * np.cos(theta)
+            y_end = y_start - t * np.sin(theta)
 
             entry = {
                 "theta": theta,
                 "x0": x_start, "y0": y_start,
                 "x1": x_end, "y1": y_end,
                 "type": 1,
-                "fan": i,
+                "fan": i+1,
                 "char": j,
                 "phi": philist_fan[j],
                 "nu": nulist_fan[j],
@@ -172,15 +244,27 @@ def compute_fan_gamma_plus(theta, x_start, y_start, N_chars, philist_fan, nulist
             }
             plot_list.append(entry)
             new_start_points.append((x_end, y_end))
-            shear_anchor=(x_end,y_end)
-            new_reflected.append(True)
+            new_reflected.append(True)  # mark as bounced
         else:
             new_start_points.append((x_start, y_start))
             new_reflected.append(reflected[j])
-    except np.linalg.LinAlgError:
-        new_start_points.append((x_start, y_start))
-        new_reflected.append(reflected[j])
-    return shear_anchor
+    #store the shear line (starts at at shear anchor, ends at start of downward char)
+    shear_entry = {
+        "theta": shear_line_angle,  # shear follows phi, not theta
+        "x0": shear_anchor[0], "y0": shear_anchor[1],
+        "x1": x_start, "y1": y_start,
+        "type": 2,
+        "fan": i+1,
+        "char": j,
+        "phi": shear_line_angle,
+        "nu": nulist_fan[j],
+        "pair_key": j,
+        "merged": False
+    }
+    plot_list.append(shear_entry)
+    
+    
+
 
 def plotting_routine(plot_list):
     '''Plotting routine for the characteristics and shear line with the style depending on the type of line'''
@@ -276,7 +360,7 @@ while shockwave==False:
         nu_new=do_MOC_minus(nu1=nulist[-1], phi1=philist[-2], phi2=philist[-1]) #following gamma -
         nulist.append(nu_new)
 
-    if len(philist)>2:
+    if len(philist)>5:
         shockwave=True
     elif len(philist)==len(nulist) and philist[-1]!=0:
         
@@ -336,16 +420,29 @@ for i in range(len(nulist) - 1):  # for each fan
         print('Fan_phi', philist_fan)
         print("Nulist fan", nulist_fan)
     for j in range(N_chars):
-        x_start, y_start = start_points[j]
-
+        x_start, y_start = [0, a]  # reset start point for each char
         theta = compute_mach_angle(nulist_fan, j, gamma, down)  # angle of characteristic line
-        
+        print('CURRENT FAN:', i, 'CHAR:', j, 'THETA (deg):', np.degrees(theta), 'PHI (deg):', np.degrees(philist_fan[j]), 'NU (deg):', np.degrees(nulist_fan[j]))
         #consider the 1st fan seperately since it originates from one point
         if i == 0:
             if debug:
                 print("First fan, j=", j)
             compute_fan_gamma_minus(theta, x_start, y_start, N_chars, philist_fan, nulist_fan, start_points, reflected,plot_list)        
         #instead of computing each up and down case, compute an up and down case and do each characterstic across both cases
+        #for upward case, store the flow phi and pm angle for each char
+        elif i%2==1: #odd fan, upward case
+            #store current characterisitc flow properties, dont do any geometry with start and end points yet
+            plot_list=store_upwards_char_info(i,j,philist_fan,nulist_fan,plot_list)
+            if debug:
+                print("Upward case, j=", j)
+        elif i%2==0: #even fan, downward case
+            compute_reflection_from_shear_line(i,j,plot_list,shear_anchor)
+            if debug:
+                print("Downward case, j=", j)
+        else:
+            print("Error in fan counting")
+            exit()
+        
 
 
 
